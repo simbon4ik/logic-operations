9ca682ed346a5874f293d412df0ea6337ee7a372
simbon4ik <karpov.ivan123@mail.ru>
2025-03-07 15:15:25 +0300

    add replacer args to values

--- prog.cpp
+++ prog.cpp
@@ -1,40 +1,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
-
+#include <algorithm>
 
 using std::string;
 using std::vector;
-using std::set;
-using std::string_view;
 
 void foo(string& str);
 bool l_not(string str);
+void replace_args(string str, vector<string> l_vars, int index);
+
 int main(){
-    
-    printf("%d\n",l_not("NOT 0"));
-    string str1 = "0 OR 0";
+   // printf("%d\n",l_not("NOT 0"));
+   // string str1 = "0 OR 0";
     string str2 = "var1 OR var2 AND NOT var3";
     str2 += ' ';    //Чтобы считать последнюю переменную
-    set<string> l_vars;
+    vector<string> l_vars;
     string l_var;
     for (char symbol : str2) {
         if (symbol == '(' || symbol == ')')
             continue;
         if (symbol == ' '){
-            if ( (l_var != "OR") && (l_var != "AND") && (l_var != "NOT") )
-                l_vars.insert(l_var);
+            if ( (l_var != "OR") && (l_var != "AND") && (l_var != "NOT") && (std::count(l_vars.begin(), l_vars.end(), l_var) == 0 ))
+                l_vars.push_back(l_var);
             l_var.clear();
             continue;
         }
         l_var += symbol;
     }
-    std::cout << "Len is " << l_vars.size() << std::endl;
-    foo(str1);
-    std::cout << str1 << std::endl;
+    std::cout << "l_vars.size " << l_vars.size() << std::endl;
+    replace_args(str2, l_vars, 0);
+    
+    //foo(str2);
     return 0;
-
 }
 
 bool l_or(string str){
@@ -49,8 +47,41 @@ bool l_not(string str){
     return not(str[4] - '0');
 }
 
+void replace_args(string str, vector<string> l_vars, int index){
+    if (index == l_vars.size()){
+        //foo()
+        std::cout << str << std::endl;
+        return;
+    }
+    string l_var = l_vars[index];
+    int size_l_var = l_var.size();
+    //Подставляем 0
+    string str_with_0 = str; //Строка для заполнения 0
+    int pos = 0;           //С какой позиции искать (для find)
+    size_t start = str_with_0.find(l_var, pos);
+    while (start != std::string::npos){
+        pos += start;
+        str_with_0.replace(start, size_l_var, "0");
+        start = str_with_0.find(l_var, pos);
+    }
+    replace_args(str_with_0, l_vars, index + 1);
+       
+    //Подставляем 1
+    pos = 0;           //С какой позиции искать (для find)
+    start = str.find(l_var, pos);
+    while (start != std::string::npos){
+        pos += start;
+        str.replace(start, size_l_var, "1");
+        start = str.find(l_var, pos);
+    }
+    replace_args(str, l_vars, index + 1);
+     
+    return;
+}
+
 void foo(string& str){
     int index = 0;
+
     while ( (index = str.find("NOT") ) != std::string::npos){
         str.replace(index , 5, std::to_string(l_not(str.substr(index, 5))));
     }
